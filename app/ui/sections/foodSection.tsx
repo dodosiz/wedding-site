a7e00a73e991eea22dff1eb5f0c10dcfad53d170
Theodosis Asvestopoulos <t.v.asvestopoulos@gmail.com>
2024-02-18 20:29:02 +0100

    Links in restaurants

--- app/ui/sections/foodSection.tsx
+++ app/ui/sections/foodSection.tsx
@@ -27,26 +27,26 @@ export function FoodSection() {
         <div>
             <h2>Ταβέρνες</h2>
             <ul>
-                <li>ταβερνάκι στην παραλία <b>Αχλάδι</b> (θαλασσινά και ψαράκια)</li>
-                <li>Αλλού Γυαλού</li>
-                <li>Ono Concept</li>
-                <li>Δυο Τζιτζίκια στ’ Αρμυρίκια (παραθαλάσσιο ταβερνάκι με θαλασσινά)</li>
-                <li>Πλακόστρωτο (στην Απάνω Μεριά που έχει θέα συγκλονιστική και έξοχα μαγειρευτά)</li>
-                <li>ξενοδοχείο Aristide</li>
-                <li>ο Μήτσος</li>
-                <li>ηλιοβασίλεμα Σύρος</li>
+                <li>ταβερνάκι στην παραλία <a href="https://www.tripadvisor.com.gr/Restaurant_Review-g6212274-d8494672-Reviews-Achladi_Restaurant-Vari_Syros_Cyclades_South_Aegean.html" target="_blank">Αχλάδι</a> (θαλασσινά και ψαράκια)</li>
+                <li><a href="https://www.tripadvisor.com.gr/Restaurant_Review-g1925453-d780363-Reviews-Allou_Yialou-Kini_Syros_Cyclades_South_Aegean.html" target="_blank">Αλλού Γυαλού</a></li>
+                <li><a href="https://www.tripadvisor.com/Restaurant_Review-g1190346-d10538290-Reviews-Ono_Concept-Poseidonia_Syros_Cyclades_South_Aegean.html" target="_blank">Ono Concept</a></li>
+                <li><a href="https://www.tripadvisor.com.gr/Restaurant_Review-g1925453-d3566771-Reviews-Dyo_Tzitzikia_Sta_Armyrikia-Kini_Syros_Cyclades_South_Aegean.html" target="_blank">Δυο Τζιτζίκια στ’ Αρμυρίκια</a> (παραθαλάσσιο ταβερνάκι με θαλασσινά)</li>
+                <li><a href="https://www.tripadvisor.com.gr/Restaurant_Review-g616163-d2465522-Reviews-To_Plakostroto-Hermoupolis_Syros_Cyclades_South_Aegean.html" target="_blank">Πλακόστρωτο</a> (στην Απάνω Μεριά που έχει θέα συγκλονιστική και έξοχα μαγειρευτά)</li>
+                <li>ξενοδοχείο <a href="https://www.tripadvisor.com/Hotel_Review-g15278394-d21131122-Reviews-Aristide_Hotel-Ermoupoli_Syros_Cyclades_South_Aegean.html" target="_blank">Aristide</a></li>
+                <li><a href="https://www.tripadvisor.com.gr/Restaurant_Review-g189435-d4702189-Reviews-Mitsos_Taverna_Syros-Syros_Cyclades_South_Aegean.html" target="_blank">ο Μήτσος</a></li>
+                <li><a href="https://www.tripadvisor.com.gr/Restaurant_Review-g1184009-d2194506-Reviews-Iliovasilema-Galissas_Syros_Cyclades_South_Aegean.html" target="_blank">ηλιοβασίλεμα</a> Σύρος</li>
             </ul>
             <h2>Brunch</h2>
             <ul>
-                <li>Le Perroquet Tranquille (φτιάχνουν τα ωραιότερα κροκ μαντάμ και κροκ μεσιέ)</li>
+                <li><a href="https://www.tripadvisor.fr/Restaurant_Review-g1190346-d8531435-Reviews-Perroquet_Tranquille-Poseidonia_Syros_Cyclades_South_Aegean.html" target="_blank">Le Perroquet Tranquille</a> (φτιάχνουν τα ωραιότερα κροκ μαντάμ και κροκ μεσιέ)</li>
             </ul>
             <h2>Ποτό</h2>
             <ul>
-                <li>Theosis (Τα πιο έμπειρα κοκτέιλ στη Σύρο)</li>
+                <li><a href="https://www.tripadvisor.com/Restaurant_Review-g3581255-d24928896-Reviews-Theosis-Ano_Siros_Syros_Cyclades_South_Aegean.html" target="_blank">Theosis</a> (Τα πιο έμπειρα κοκτέιλ στη Σύρο)</li>
             </ul>
             <h2>Γλυκό</h2>
             <ul>
-                <li>Αθυμαρίτη (Τα ροδίνια είναι τα συριανά, αριστοκρατικά αμυγδαλωτά, στου Αθυμαρίτη φτιάχνουν την απόλυτη εκδοχή)</li>
+                <li><a href="https://www.tripadvisor.com.gr/Restaurant_Review-g616163-d10814495-Reviews-M_I_Athymaritis-Hermoupolis_Syros_Cyclades_South_Aegean.html" target="_blank">Αθυμαρίτη</a> (Τα ροδίνια είναι τα συριανά, αριστοκρατικά αμυγδαλωτά, στου Αθυμαρίτη φτιάχνουν την απόλυτη εκδοχή)</li>
             </ul>
         </div>
         <div className={styles.side_gif}>
